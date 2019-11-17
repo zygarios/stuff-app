34626595ea4c6a4a3adc016cf4139b288fc43d29
Bartosz Zygmunt <40625750+zygarios@users.noreply.github.com>
2019-11-17 13:09:20 +0100

    navbar update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 import "../Sass/Navbar.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faPowerOff } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faPowerOff,
+  faUserTag,
+  faBookmark
+} from "@fortawesome/free-solid-svg-icons";
 import { Redirect } from "react-router";
 
 function HomePanel() {
   const [isLogged, setIsLogged] = useState(true);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const handleLogout = () => {
     localStorage.removeItem("access_token");
     return <Redirect to="/login" />;
@@ -13,12 +20,30 @@ function HomePanel() {
   return (
     <nav className="navbar">
       <div className="navbar__container">
-        <FontAwesomeIcon icon={faBars} className="navbar__hamburger-btn" />
-        <FontAwesomeIcon
-          icon={faPowerOff}
-          className="navbar__logout-btn"
-          onClick={() => setIsLogged(false)}
-        />
+        <h1 class="navbar__title-logo">
+          Zakładka
+          <FontAwesomeIcon className="navbar__title-icon" icon={faBookmark} />
+        </h1>
+        <div className="navbar__menu">
+          {isMenuOpen && (
+            <>
+              <span className="navbar__user-settings-btn">
+                <FontAwesomeIcon icon={faUserTag} />
+              </span>
+              <span className="navbar__logout-btn">
+                <FontAwesomeIcon
+                  icon={faPowerOff}
+                  onClick={() => setIsLogged(false)}
+                />
+              </span>
+            </>
+          )}
+          <span
+            className={`navbar__hamburger-btn ${isMenuOpen && "active-select"}`}
+            onClick={() => setIsMenuOpen(prev => !prev)}>
+            <FontAwesomeIcon icon={faBars} />
+          </span>
+        </div>
         {!isLogged && handleLogout()}
         {/* <div className="navbar__search" />
         <div className="navbar__settings" /> */}
